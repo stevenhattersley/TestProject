26b9bfce1d082b313b691c5d43a8781a391daa7f
sehattersley <s.e.hattersley@gmail.com>
2018-09-02 21:10:06 +0100

    New files added

---
+++ Untitled-1.py
@@ -0,0 +1,6 @@
+import fileTwo
+#test file
+
+print("hello I am file 1")
+fileTwo.FunctionOne()
+print("Script finished")
---
+++ fileTwo.py
@@ -0,0 +1,3 @@
+def FunctionOne():
+    print("hello I am the function from file two")
+    return

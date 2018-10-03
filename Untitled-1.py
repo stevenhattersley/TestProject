382162228b1c61d737c75c42eb231ce353594487
sehattersley <s.e.hattersley@gmail.com>
2018-10-03 11:52:48 +0100

    sdrfgsdfgdsfdsfg

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,6 +1,8 @@
 import fileTwo
-#test file
-
+# test file
+msg = "my message"
 print("hello I am file 1")
+print("This is really long!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+print msg
 fileTwo.FunctionOne()
 print("Script finished")
